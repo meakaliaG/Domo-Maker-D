b2ab27d3dc70b7da8328d8246a853ffd2876f49f
meakaliaG <mlg2225@g.rit.edu>
2025-11-10 23:10:52 -0500

    react complete

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -5,10 +5,6 @@ const loginPage = (req, res) => {
     return res.render('login');
 };
 
-const signupPage = (req, res) => {
-    return res.render('signup');
-};
-
 const logout = (req, res) => {
     req.session.destroy();
     return res.redirect('/');
@@ -62,7 +58,6 @@ const signup = async (req, res) => {
 
 module.exports = {
     loginPage,
-    signupPage,
     login,
     logout,
     signup,
