2048ae5898737f79b6e63bc0538ac81dc758a535
meakaliaG <mlg2225@g.rit.edu>
2025-11-16 22:37:05 -0500

    fixed dbURI string typo

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const router = require('./router.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const dbURI = process.env.MOGODB_URI || 'mongodb://localhost/DomoMaker';
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
 mongoose.connect(dbURI).catch((err) => {
     if(err) {
         console.log('Could not connect to database');
